ed0d2f6ad7ba07561b6715a89c2622c87bef5888
Omar ZG <omar.zg@dominio.com>
2020-05-06 21:07:51 -0500

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    // console.log('Servicio de InfoPagina Listo...');
+    this.cargaInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargaInfo() {
+
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) =>  {
-
     this.cargada = true;
     this.info = resp;
-    console.log( resp );
 
     });
 
   }
+
+  private cargarEquipo() {
+    // Leer archivo JSON
+    this.http.get('https://angula-base.firebaseio.com/equipo.json').subscribe( ( resp: any[] ) =>  {
+
+      this.equipo = resp;
+      console.log( resp );
+
+      });
+  }
+
 }
